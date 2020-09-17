15927f8fc167f4c8ffce1a757c88689e7948b149
Thalis Duarte <thalis_galeno@hotmail.com.br>
2020-09-17 18:46:18 -0300

    obtendo user por id

--- src/main/java/com/thalistdg/workshopmongo/services/UserService.java
+++ src/main/java/com/thalistdg/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.thalistdg.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thalistdg.workshopmongo.domain.User;
 import com.thalistdg.workshopmongo.repository.UserRepository;
+import com.thalistdg.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
 	}
 }
