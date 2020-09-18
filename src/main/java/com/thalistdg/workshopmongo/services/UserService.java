ea82bad17f2b36cb3885161d7868e20073859590
Thalis Duarte <thalis_galeno@hotmail.com.br>
2020-09-18 18:50:41 -0300

    atualização de usuario com put

--- src/main/java/com/thalistdg/workshopmongo/services/UserService.java
+++ src/main/java/com/thalistdg/workshopmongo/services/UserService.java
@@ -35,6 +35,17 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setEmail(obj.getEmail());
+		newObj.setName(obj.getName());		
+	}
+
 	public User fromDTO(UserDTO obj) {
 		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
